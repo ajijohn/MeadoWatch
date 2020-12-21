3685da9df330258c37857425ae740b2c805e81eb
Janneke <jhrl@uw.edu>
2020-12-21 17:38:34 +0100

    Changes made: - all functions now to HRL_phenology_functions - model fitting by species uses a new simplified MLE function, and uses multiple optimizers - model fitting by year / species / plot are mostly very minor changes

--- HRL_phenology_functions.R
+++ HRL_phenology_functions.R
@@ -23,7 +23,7 @@ nullfit <- function (param){
 # is fit with a logit transformation (similar to Theobald et al 2017, 
 # Sethi et al 2020; albeit with parameters that do NOT vary with climate). 
 # Also requires yes / no observations. This function can be fit to 
-# yes / no observations of phenology (phenophase) observed on DOY
+# yes / no observations of phenology (phenophase) observed by DOY
 
 curvefit_perplot <- function (param){ #curve fitting function for mle
   peakp  <- param[1] #in days, the time of peak phenophase
@@ -35,6 +35,84 @@ curvefit_perplot <- function (param){ #curve fitting function for mle
 }
 
 
+# Phenological mle model per species, fit to yes / no data from multiple plots
+# Also a binomial likelihood model that assuming that flowering probability
+# for a species in a plot is a function of several parameters. The probability 
+# of flowering is assumed to be unimodal with peak flowering in a plot 
+# depending on snowmelt date at that plot (a simple linear relationship
+# described by an intercept and slope parameter). The parameters describing
+# the range and maximum are also fit per species
+
+curvefit_species <- function (param){ #curve fitting function for mle
+  intpeak  <- param[1]
+  slopepeak <- param[2]
+  rangep <- param[3]
+  maxp <- param[4]
+  peakp <- intpeak + slopepeak*SDD
+  pred <- inv.logit(rangep * (days - peakp)^2 + maxp)
+  llik     <- dbinom(phenophase,1,pred, log=TRUE)
+  return(-sum(llik))
+}
+
+
+# Estimate peak flowering as a function of peak, duration and max parameters.
+# This function takes as input DOY (days since January 1) and 3 parameters 
+# describing peak, range and duration of flowering; and returns the 
+# probability of flowering (primarily used for plotting). 
+
+predflower <- function (xx, param){ 
+  days    <- xx
+  peakp  <- param[1]
+  rangep <- param[2]
+  maxp   <- param[3]
+  pred <- inv.logit(rangep * (days - peakp)^2 + maxp)
+  return(pred)
+}
+
+# Estimate the probability of observing a flower within a MeadoWatch plot.
+# This function takes as input DOY (days since January 1) and 4 parameters 
+# describing the earliest peak flowering observed along the trail, the latest
+# peak flowering observed along the trail, and parameters describing the duration
+# and maximum probability of flowering. 
+
+predflowertrail <- function (xx, param){ 
+  days    <- xx
+  peakearly  <- param[1]
+  peaklate  <- param[2]
+  range <- param[3]
+  max   <- param[4]
+  predmax <- inv.logit(max)
+  ptrailflower <- rep(predmax, time=length(days))
+  predearly <- inv.logit(range * (days - peakearly)^2 + max)
+  predlate <- inv.logit(range * (days - peaklate)^2 + max)
+  ptrailflower[days[]<peakearly] <- predearly[days[]<peakearly]
+  ptrailflower[days[]>peaklate] <- predlate[days[]>peaklate]
+  ptrailflower[ptrailflower<0.005] <- 0
+  return(ptrailflower)
+}
+
+# Similar to the function above, but calculates richness.
+# This function takes as input DOY (days since January 1) and 4 parameters 
+# describing the earliest peak flowering observed along the trail, the latest
+# peak flowering observed along the trail, and parameters describing the duration
+# and maximum probability of flowering. 
+
+predrichtrail <- function (xx, param){ 
+  days    <- xx
+  peakearly  <- param[1]
+  peaklate  <- param[2]
+  predmax <- inv.logit(max)
+  predearly <- inv.logit(range * (days - peakearly)^2 + max)
+  predlate <- inv.logit(range * (days - peaklate)^2 + max)
+  pyesnoflower <- rep(1, time=length(days))
+  pyesnoflower[predearly[]<0.5*predmax & days[]<peakearly] <- 0
+  pyesnoflower[predearly[]<0.5*predmax & days[]>peaklate] <- 0
+  return(ptrailflower)
+}
+
+
+#########FOR POSTERITY - no longer in use
+
 #Fits model per species 
 #peak flowering varies with snowmelt, and plot/trail specific range and max
 #Should be fit per species (but all data for each speices)
@@ -45,8 +123,6 @@ curvefit_perplot <- function (param){ #curve fitting function for mle
 # relationship described by an intercept and slope parameter). Parameters 
 # describing the range and maximum flowering are allowed to vary by year and
 # and trail. This function is fit to all data from a species.
-
-
 curvefit_allplot <- function (param){ #curve fitting function for mle
   intpeak  <- param[1]
   slopepeak <- param[2]
@@ -59,18 +135,3 @@ curvefit_allplot <- function (param){ #curve fitting function for mle
   llik     <- dbinom(phenophase,1,pred, log=TRUE)
   return(-sum(llik))
 }
-
-
-# This function takes as input DOY (days since January 1) and 3 parameters 
-# describing peak, range and duration of flowering; and returns the 
-# probability of flowering. This is primarily used for graphing.
-
-predflower <- function (xx, param){ 
-  days    <- xx
-  peakp  <- param[1]
-  rangep <- param[2]
-  maxp   <- param[3]
-  pred <- inv.logit(rangep * (days - peakp)^2 + maxp)
-  return(pred)
-}
-
